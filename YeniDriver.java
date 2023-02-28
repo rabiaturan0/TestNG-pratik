68465573f5848343f856ce506bb25766310256ec
rabiaturan0 <rbtrn229@gmail.com>
2023-02-28 11:28:18 +0300

    28/02/23

---
+++ YeniDriver.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class YeniDriver {
+    @Test(groups = "gp1")
+    public void testName() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_url"));
+        Driver.getDriver().get("https://techproeducation.com");
+        Driver.closeDriver();
+        //Driver.getDriver().get("https://facebook.com");
+
+    }
+}
